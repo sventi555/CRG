01adbb5bcfd1781b7f6a6eccdc0e0031c5ea9976
Stephen <sventi555@gmail.com>
2018-11-11 15:08:20 -0500

    fixing articles per page

--- website/views.py
+++ website/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.core.paginator import Paginator
 from .models import *
 
-ARTICLES_PER_PAGE = 1
+ARTICLES_PER_PAGE = 10
 
 def home(request):
     latest = []
